e95d7d5a1f80b6e9b8e838b2c0f7b7ea862b521b
LDFan <48767375+LDFan@users.noreply.github.com>
2024-12-15 15:07:13 +0900

    Update ProfilePage.jsx

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -26,7 +26,7 @@ const ProfilePage = () => {
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
-            <h1 className="text-2xl font-semibold ">Profile</h1>
+            <h1 className="text-2xl font-semibold ">个人资料</h1>
             <p className="mt-2">Your profile information</p>
           </div>
 
@@ -84,7 +84,7 @@ const ProfilePage = () => {
           </div>
 
           <div className="mt-6 bg-base-300 rounded-xl p-6">
-            <h2 className="text-lg font-medium  mb-4">Account Information</h2>
+            <h2 className="text-lg font-medium  mb-4">个人信息</h2>
             <div className="space-y-3 text-sm">
               <div className="flex items-center justify-between py-2 border-b border-zinc-700">
                 <span>Member Since</span>
